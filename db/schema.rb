f49e10a8b0393b87cd72de020244fcc9ddb2c0e2
shutheonetokyo <ss1030ss@icloud.com>
2021-03-28 14:47:10 +0900

    usersテーブルの削除

--- db/schema.rb
+++ db/schema.rb
@@ -22,11 +22,4 @@ ActiveRecord::Schema.define(version: 2021_03_27_064851) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "title"
-    t.string "content"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
 end
